9345513f68b17ff9ae0b7c64462782034b898dd3
Adam Bouzourene <adam1@172-27-222-24.DYNAPOOL.NYU.EDU>
2017-02-07 13:45:20 -0500

    added textured polygons

--- Xcode/NYUCodebase/main.cpp
+++ Xcode/NYUCodebase/main.cpp
@@ -4,6 +4,8 @@
 #include <SDL.h>
 #include <SDL_opengl.h>
 #include <SDL_image.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
 
 #include "ShaderProgram.h"
 
@@ -14,6 +16,23 @@
 #endif
 
 SDL_Window* displayWindow;
+
+GLuint LoadTexture(const char *filePath) {
+    int w,h,comp;
+    unsigned char* image = stbi_load(filePath, &w, &h, &comp, STBI_rgb_alpha);
+    if(image == NULL) {
+        std::cout << "Unable to load image. Make sure the path is correct\n";
+        assert(false);
+    }
+    GLuint retTexture;
+    glGenTextures(1, &retTexture);
+    glBindTexture(GL_TEXTURE_2D, retTexture);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, w, h, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    stbi_image_free(image);
+    return retTexture;
+}
 
 int main(int argc, char *argv[])
 {
@@ -30,13 +49,18 @@ int main(int argc, char *argv[])
     
     glViewport(0, 0, 640, 360);
     
-    ShaderProgram program(RESOURCE_FOLDER"vertex.glsl", RESOURCE_FOLDER"fragment.glsl");
+    ShaderProgram program(RESOURCE_FOLDER"vertex_textured.glsl", RESOURCE_FOLDER"fragment_textured.glsl");
+    
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     
     Matrix projectionMatrix;
     Matrix modelMatrix;
     Matrix viewMatrix;
     
     projectionMatrix.setOrthoProjection(-3.55, 3.55, -2.0f, 2.0f, -1.0f, 1.0f);
+    
+    GLuint ufoTexture = LoadTexture(RESOURCE_FOLDER"ufo.png");
     
     glUseProgram(program.programID);
                           
@@ -48,15 +72,22 @@ int main(int argc, char *argv[])
         modelMatrix.Rotate(45.0 * (3.1415926 / 70));
         modelMatrix.Translate(-0.7, 1.0, 0.0);
         
+        program.setModelMatrix(modelMatrix);
         program.setProjectionMatrix(projectionMatrix);
         program.setViewMatrix(viewMatrix);
         
-        float vertices[] = {0.5f, -0.5f, 0.0f, 1.0f, -0.5f, -0.5f};
-        
+        glBindTexture(GL_TEXTURE_2D, ufoTexture);
+        float vertices[] = {-0.5, -0.5, 0.5, -0.5, 0.5, 0.5, -0.5, -0.5, 0.5, 0.5, -0.5, 0.5};
         glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, vertices);
         glEnableVertexAttribArray(program.positionAttribute);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        
+        float texCoords[] = {0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0};
+        glVertexAttribPointer(program.texCoordAttribute, 2, GL_FLOAT, false, 0, texCoords);
+        glEnableVertexAttribArray(program.texCoordAttribute);
+        
+        glDrawArrays(GL_TRIANGLES, 0, 6);
         glDisableVertexAttribArray(program.positionAttribute);
+        glDisableVertexAttribArray(program.texCoordAttribute);
         
         SDL_GL_SwapWindow(displayWindow);
         
